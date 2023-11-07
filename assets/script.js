5d9f4f20a8fc862ecf2ff3880d8d971766f57b8c
Brandon <beegee54321@gmail.com>
2023-11-06 16:21:22 -0800

    added more js still needs debugging html and css

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,36 @@ var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 $(document).ready(function() {
+  $(".saveBtn").on("click", function() {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  })
+
+  function timeTracker() {
+    var timeNow = moment().hour();
+
+    $(".time-block").each(function() {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    })
+  }
+
+
   
  });
 
